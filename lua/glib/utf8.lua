06288245d1a5e0e86d793c0b8df8384a4bb3e9ae
!cake <cakenotfound@googlemail.com>
2012-09-15 20:41:15 +0000

    [GLib] GLib.UTF8 optimization.

--- lua/glib/utf8.lua
+++ lua/glib/utf8.lua
@@ -1,34 +1,43 @@
 GLib.UTF8 = {}
 GLib.UTF8.Characters = {}
 
+local math_floor    = math.floor
+local string_byte   = string.byte
+local string_char   = string.char
+local string_len    = string.len
+local string_find   = string.find
+local string_format = string.format
+local string_gsub   = string.gsub
+local string_sub    = string.sub
+
 function GLib.UTF8.Byte (char, offset)
 	if char == "" then return -1 end
 	offset = offset or 1
 	
-	local byte = string.byte (char, offset)
+	local byte = string_byte (char, offset)
 	local length = 1
 	if byte >= 128 then
 		if byte >= 240 then
 			-- 4 byte sequence
 			length = 4
-			if string.len (char) < 4 then return -1, length end
+			if string_len (char) < 4 then return -1, length end
 			byte = (byte & 7) * 262144
-			byte = byte + (string.byte (char, offset + 1) & 63) * 4096
-			byte = byte + (string.byte (char, offset + 2) & 63) * 64
-			byte = byte + (string.byte (char, offset + 3) & 63)
+			byte = byte + (string_byte (char, offset + 1) & 63) * 4096
+			byte = byte + (string_byte (char, offset + 2) & 63) * 64
+			byte = byte + (string_byte (char, offset + 3) & 63)
 		elseif byte >= 224 then
 			-- 3 byte sequence
 			length = 3
-			if string.len (char) < 3 then return -1, length end
+			if string_len (char) < 3 then return -1, length end
 			byte = (byte & 15) * 4096
-			byte = byte + (string.byte (char, offset + 1) & 63) * 64
-			byte = byte + (string.byte (char, offset + 2) & 63)
+			byte = byte + (string_byte (char, offset + 1) & 63) * 64
+			byte = byte + (string_byte (char, offset + 2) & 63)
 		elseif byte >= 192 then
 			-- 2 byte sequence
 			length = 2
-			if string.len (char) < 2 then return -1, length end
+			if string_len (char) < 2 then return -1, length end
 			byte = (byte & 31) * 64
-			byte = byte + (string.byte (char, offset + 1) & 63)
+			byte = byte + (string_byte (char, offset + 1) & 63)
 		else
 			-- invalid sequence
 			byte = -1
@@ -42,28 +51,28 @@ function GLib.UTF8.Char (byte)
 	if byte < 0 then
 		utf8 = ""
 	elseif byte <= 127 then
-		utf8 = string.char (byte)
+		utf8 = string_char (byte)
 	elseif byte < 2048 then
-		utf8 = string.format ("%c%c", 192 + math.floor (byte / 64), 128 + (byte & 63))
+		utf8 = string_format ("%c%c",     192 + math_floor (byte / 64),     128 + (byte & 63))
 	elseif byte < 65536 then
-		utf8 = string.format ("%c%c%c", 224 + math.floor (byte / 4096), 128 + (math.floor (byte / 64) & 63), 128 + (byte & 63))
+		utf8 = string_format ("%c%c%c",   224 + math_floor (byte / 4096),   128 + (math_floor (byte / 64) & 63),   128 + (byte & 63))
 	elseif byte < 2097152 then
-		utf8 = string.format ("%c%c%c%c", 240 + math.floor (byte / 262144), 128 + (math.floor(byte / 4096) & 63), 128 + (math.floor (byte / 64) & 63), 128 + (byte & 63))
+		utf8 = string_format ("%c%c%c%c", 240 + math_floor (byte / 262144), 128 + (math_floor (byte / 4096) & 63), 128 + (math_floor (byte / 64) & 63), 128 + (byte & 63))
 	end
 	return utf8
 end
 
 function GLib.UTF8.ContainsSequences (str, offset)
-	return string.find (str, "[\192-\255]", offset) and true or false
+	return string_find (str, "[\192-\255]", offset) and true or false
 end
 
 function GLib.UTF8.GetSequenceStart (str, offset)
 	if offset <= 0 then return 1 end
-	if offset > str:len () then offset = str:len () end
+	if offset > string_len (str) then offset = string_len (str) end
 	
 	local startOffset = offset
 	while startOffset >= 1 do
-		byte = string.byte (str, startOffset)
+		byte = string_byte (str, startOffset)
 		
 		-- Either a single byte sequence or
 		-- an improperly started multi byte sequence, in which case it's treated as one byte long
@@ -82,19 +91,19 @@ function GLib.UTF8.Iterator (str, offset)
 	if offset <= 0 then offset = 1 end
 	
 	return function ()
-		if offset > string.len (str) then return nil, nil end
+		if offset > string_len (str) then return nil, nil end
 		
 		local length
 		
 		-- Inline expansion of GLib.UTF8.SequenceLength (str, offset)
-		local byte = string.byte (str, offset)
+		local byte = string_byte (str, offset)
 		if not byte then length = 0
 		elseif byte >= 240 then length = 4
 		elseif byte >= 224 then length = 3
 		elseif byte >= 192 then length = 2
 		else length = 1 end
 		
-		local character = string.sub (str, offset, offset + length - 1)
+		local character = string_sub (str, offset, offset + length - 1)
 		local lastOffset = offset
 		offset = offset + length
 		return character, lastOffset
@@ -102,7 +111,7 @@ function GLib.UTF8.Iterator (str, offset)
 end
 
 function GLib.UTF8.Length (str)
-	local _, length = string.gsub (str, "[^\128-\191]", "")
+	local _, length = string_gsub (str, "[^\128-\191]", "")
 	return length
 end
 
@@ -111,19 +120,19 @@ function GLib.UTF8.NextChar (str, offset)
 	if offset <= 0 then offset = 1 end
 	
 	local length = GLib.UTF8.SequenceLength (str, offset)
-	return string.sub (str, offset, offset + length - 1), offset + length
+	return string_sub (str, offset, offset + length - 1), offset + length
 end
 
 function GLib.UTF8.PreviousChar (str, offset)
-	offset = offset or (string.len (str) + 1)
+	offset = offset or (string_len (str) + 1)
 	if offset <= 1 then return "", 0 end
 	local startOffset = GLib.UTF8.GetSequenceStart (str, offset - 1)
 	local length = GLib.UTF8.SequenceLength (str, startOffset)
-	return string.sub (str, startOffset, startOffset + length - 1), startOffset
+	return string_sub (str, startOffset, startOffset + length - 1), startOffset
 end
 
 function GLib.UTF8.SequenceLength (str, offset)
-	local byte = string.byte (str, offset)
+	local byte = string_byte (str, offset)
 	if not byte then return 0
 	elseif byte >= 240 then return 4
 	elseif byte >= 224 then return 3
@@ -136,7 +145,7 @@ function GLib.UTF8.SplitAt (str, char)
 	local offsetChar = 1 -- character index corresponding to offset
 	while c ~= "" do
 		if offsetChar >= char then
-			return string.sub (str, 1, offset - 1), string.sub (str, offset)
+			return string_sub (str, 1, offset - 1), string_sub (str, offset)
 		end
 		c, offset = GLib.UTF8.NextChar (str, offset)
 		offsetChar = offsetChar + 1
@@ -154,10 +163,10 @@ function GLib.UTF8.SubOffset (str, offset, startCharacter, endCharacter)
 	if offset < 1 then offset = 1 end
 	local charactersSkipped = offset - 1
 	
-	if startCharacter > str:len () - charactersSkipped then return "" end
+	if startCharacter > string_len (str) - charactersSkipped then return "" end
 	if endCharacter then
 		if endCharacter < startCharacter then return "" end
-		if endCharacter > str:len () - charactersSkipped then endCharacter = nil end
+		if endCharacter > string_len (str) - charactersSkipped then endCharacter = nil end
 	end
 
 	local iterator = GLib.UTF8.Iterator (str, offset)
@@ -172,7 +181,7 @@ function GLib.UTF8.SubOffset (str, offset, startCharacter, endCharacter)
 	if not startOffset then return "" end
 	nextCharacter = nextCharacter + 1
 	if not endCharacter then
-		return str:sub (startOffset)
+		return string_sub (str, startOffset)
 	end
 	
 	while nextCharacter <= endCharacter do
@@ -182,9 +191,9 @@ function GLib.UTF8.SubOffset (str, offset, startCharacter, endCharacter)
 	
 	local _, endOffset = iterator ()
 	if endOffset then
-		return str:sub (startOffset, endOffset - 1)
+		return string_sub (str, startOffset, endOffset - 1)
 	else
-		return str:sub (startOffset)
+		return string_sub (str, startOffset)
 	end
 end
 
