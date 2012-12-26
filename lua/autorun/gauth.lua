0d8e80d59b2494aca1e53357839ba3eea4b665ec
!cake <cakenotfound@googlemail.com>
2012-12-26 22:45:20 +0000

    [GAuth] Fixed autorun. [VFS] Fixed autorun.

--- lua/autorun/gauth.lua
+++ lua/autorun/gauth.lua
@@ -1,3 +1,5 @@
-if CLIENT and not file.Exists ("gauth/gauth.lua", "LCL") then return end
-if CLIENT and not GetConVar ("sv_allowcslua"):GetBool () then return end
-include ("gauth/gauth.lua")
+if SERVER or
+   file.Exists ("gauth/gauth.lua", "LUA") or
+   file.Exists ("gauth/gauth.lua", "LCL") and GetConVar ("sv_allowcslua"):GetBool () then
+	include ("gauth/gauth.lua")
+end
--- lua/autorun/vfs.lua
+++ lua/autorun/vfs.lua
@@ -1,3 +1,5 @@
-if CLIENT and not file.Exists ("vfs/vfs.lua", "LCL") then return end
-if CLIENT and not GetConVar ("sv_allowcslua"):GetBool () then return end
-include ("vfs/vfs.lua")
+if SERVER or
+   file.Exists ("vfs/vfs.lua", "LUA") or
+   file.Exists ("vfs/vfs.lua", "LCL") and GetConVar ("sv_allowcslua"):GetBool () then
+	include ("vfs/vfs.lua")
+end
